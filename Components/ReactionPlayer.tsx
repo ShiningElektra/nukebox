7e8d28f08ec139cf1e9aee1d43e86ea1ad2300e1
ShiningElektra <elektrathanos@googlemail.com>
2021-02-22 09:19:02 +0100

    ✨ Get like Button work

--- Components/ReactionPlayer.tsx
+++ Components/ReactionPlayer.tsx
@@ -1,6 +1,12 @@
 import styles from "../styles/ReactionPlayer.module.css";
+import React, { useState } from "react";
 
 export default function ReactionPlayer() {
+  const [like, setLike] = useState(false);
+
+  const LikeButton = () => {
+    setLike(!like);
+  };
   return (
     <div className={styles.reaction}>
       <img
@@ -8,7 +14,11 @@ export default function ReactionPlayer() {
         src="/Downlaod_Icon.svg"
         onClick={null}
       />
-      <img className={styles.like} src="/Like_Icon.svg" onClick={null} />
+      <img
+        className={styles.like}
+        onClick={LikeButton}
+        src={!like ? "/Like_Icon.svg" : "/Liked.svg"}
+      />
       <img className={styles.volume} src="/Volume_Icon.svg" onClick={null} />
     </div>
   );
