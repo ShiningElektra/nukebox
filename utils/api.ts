b624fd587b34118cfa15adb69432ca23400f2466
ShiningElektra <elektrathanos@googlemail.com>
2021-02-25 15:53:39 +0100

    ✨  Add Delete Track Button to delete from API

--- utils/api.ts
+++ utils/api.ts
@@ -18,3 +18,12 @@ export async function getTracks(): Promise<APITrack[]> {
 export async function getTrack(id: string): Promise<APITrack> {
   return await fetchURL<APITrack>(`/api/tracks/${id}`);
 }
+// export async function deleteTrack(id: string): Promise<APITrack> {
+
+// }
+export async function deleteTrack(id: string) {
+  await fetch(`/api/tracks/${id}`, {
+    method: "DELETE",
+  });
+  return;
+}
