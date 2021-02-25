03abfd6925c48ace6de142eb424a9aed8b293114
ShiningElektra <elektrathanos@googlemail.com>
2021-02-25 15:01:08 +0100

    ✨ allow to read and write in API

--- pages/api/tracks/[id].ts
+++ pages/api/tracks/[id].ts
@@ -1,15 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
+import { readDb, writeDb } from "../../../server/db";
 import db from "../../../server/db.json";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
-  const track = db.tracks.find((track) => track.id === id);
-  if (!track) {
-    return res.status(404).json({
-      status: 404,
-      error: "Track not found",
+  if (req.method === "GET") {
+    const track = db.tracks.find((track) => track.id === id);
+    if (!track) {
+      return res.status(404).json({
+        status: 404,
+        error: "Track not found",
+      });
+    }
+    res.status(200).json(track);
+  }
+  if (req.method === "DELETE") {
+    const db = await readDb();
+    db.tracks = db.tracks.filter((track) => track.id !== id);
+
+    await writeDb(db);
+
+    console.log("DELETE NOW!");
+    return res.status(200).json({
+      status: 200,
+      message: "DELETED",
     });
   }
-  res.status(200).json(track);
+  return res.status(405).end();
 };
